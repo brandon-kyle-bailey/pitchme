d84c947d57a358e10aed0c7d90d4015b36fcc5bc
brandon-kyle-bailey <brandonkylebailey@outlook.com>
2026-03-18 12:06:50 -0300

    chore: add some api stuff and web stuff

--- apps/web/src/lib/auth.ts
+++ apps/web/src/lib/auth.ts
@@ -4,4 +4,14 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    },
+    linkedin: {
+      clientId: process.env.LINKEDIN_CLIENT_ID as string,
+      clientSecret: process.env.LINKEDIN_CLIENT_SECRET as string,
+    },
+  },
 });
