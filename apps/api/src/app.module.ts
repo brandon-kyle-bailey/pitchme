8c08a71a5769b6facb94d62c87f3b381bf117a30
brandon-kyle-bailey <brandonkylebailey@outlook.com>
2026-03-21 16:29:32 -0300

    chore: fix containerization and api modules

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,65 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import KeyvRedis from '@keyv/redis';
+import { CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
-import { UsersModule } from './modules/users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CacheableMemory } from 'cacheable';
+import { Keyv } from 'keyv';
 import { AuthModule } from './modules/auth/auth.module';
 import { HealthModule } from './modules/health/health.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, HealthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      expandVariables: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        const production = config.get('NODE_ENV') === 'production';
+        return {
+          type: 'postgres',
+          host: config.get<string>('DATABASE_HOST', 'db'),
+          port: config.get<number>('DATABASE_PORT', 5432),
+          username: config.get<string>('DATABASE_USER', 'postgres'),
+          password: config.get<string>('DATABASE_PASSWORD', 'postgres'),
+          database: config.get<string>('DATABASE_NAME', 'core'),
+          synchronize: !production,
+          autoLoadEntities: true,
+          migrations: [],
+          migrationsRun: production,
+          retryAttempts: 10,
+          retryDelay: 5000,
+        };
+      },
+    }),
+    CacheModule.registerAsync({
+      isGlobal: true,
+      inject: [ConfigService],
+      // eslint-disable-next-line @typescript-eslint/require-await
+      useFactory: async (configService: ConfigService) => {
+        const redisUrl = configService.get<string>(
+          'REDIS_URL',
+          'redis://localhost:6379',
+        );
+        return {
+          stores: [
+            new Keyv({ store: new KeyvRedis(redisUrl), ttl: 5_000 }),
+            new Keyv({
+              store: new CacheableMemory({ ttl: 5_000, lruSize: 5_000 }),
+            }),
+          ],
+        };
+      },
+    }),
+    UsersModule,
+    AuthModule,
+    HealthModule,
+  ],
   controllers: [],
   providers: [],
 })
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,40 @@
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import type { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import compression from 'compression';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 80);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const configService = app.get<ConfigService>(ConfigService);
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+  app.setGlobalPrefix('api/core');
+  app.use(compression());
+  app.use(helmet());
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.enableCors({
+    origin: configService.get<string>('ALLOWED_ORIGINS'),
+    credentials: true,
+  });
+  app.set('trust proxy', true);
+
+  if (process.env.NODE_ENV !== 'production') {
+    const config = new DocumentBuilder()
+      .setTitle('Core API')
+      .setDescription('The Core API.')
+      .setVersion('1.0')
+      .addTag('core')
+      .addBearerAuth()
+      .build();
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, documentFactory);
+  }
+
+  await app.listen(configService.get<string>('PORT') ?? 80);
 }
 void bootstrap();
